a9dfb9f2e15b6f4dfa81f81e65265cd3df3c45a8
aleromrod1 <aleromrod1@us.es>
2018-03-20 20:19:42 +0100

    Generación dinámica del notebook

--- ClimbCodeDjango/climbcode/web/views.py
+++ ClimbCodeDjango/climbcode/web/views.py
@@ -11,5 +11,30 @@ def index(request):
 
 def notebookv1(request):
 	template = loader.get_template('web/notebookv1.html')
-	context = {}
+
+	cajas = []
+	parameters = []
+	parameters.append(Parameters("1"))
+	parameters.append(Parameters("2"))
+	parameters.append(Parameters("3"))
+
+	cajas.append(Box("Texto","Textooooooooooooo",1))
+	cajas.append(Box("Ilustracion","https://media1.tenor.com/images/039ef9592d0eb63596f695c1f65c2502/tenor.gif?itemid=9989174",2,parameters))
+	cajas.append(Box("Codigo","2+1;",3))
+	context = {
+		'cajas' : cajas,
+	}
+
 	return HttpResponse(template.render(context, request))
+
+
+class Box():
+	def __init__(self, tipo=None, content=None, order=None, parameters=[]):
+		self.tipo = tipo
+		self.content = content
+		self.order = order
+		self.parameters = parameters
+
+class Parameters():
+	def __init__(self, id=None):
+		self.id = id

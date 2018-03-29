186aeefb3234ac41b47ccd6416c08913bf8eb7b8
Andrés Fernández Alés <andferale@alum.us.es>
2018-03-30 00:49:09 +0200

    Prueba deploy (1)

--- ClimbCodeDjango/climbcode/climbcode/settings.py
+++ ClimbCodeDjango/climbcode/climbcode/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import posixpath
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

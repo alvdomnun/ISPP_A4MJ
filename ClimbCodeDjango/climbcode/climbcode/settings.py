97062403d219516d4f06f18a119f884d2c8cc050
Andrés Fernández Alés <andferale@alum.us.es>
2018-03-29 17:33:27 +0200

    Fix (2) settings para deploy

--- ClimbCodeDjango/climbcode/climbcode/settings.py
+++ ClimbCodeDjango/climbcode/climbcode/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'climbcode',
-        'USER': 'postgres',
+        'USER': 'climbcode',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',

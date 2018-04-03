ed3e6dc7b2e2181a191b7d86da3d65970a643011
Andrés Fernández Alés <andferale@alum.us.es>
2018-04-03 14:37:54 +0200

    Update deploy.bat

--- ClimbCodeDjango/Scripts/deploy/deploy.bat
+++ ClimbCodeDjango/Scripts/deploy/deploy.bat
@@ -1,1 +1,1 @@
-plink.exe 159.89.103.249:22 -l root -pw 8q6u6szfx -m deploy.txt -t
+plink.exe 159.89.103.249:22 -l root -pw djiabhsaAQQQLAgiytf7te8y29hin@hdASDWWWQAPXZydag128739yu#@|#|@#|@=92%|@#€2321j3hSS12i312973|#@#@¡2#|@#|hsjdvaisDAdonk|@ -m deploy.txt -t

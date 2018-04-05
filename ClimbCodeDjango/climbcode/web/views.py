27538ab0b4a7b7d735e9cff815add2d9153a8f40
aleromrod1 <aleromrod1@us.es>
2018-04-05 22:01:43 +0200

    Eliminación de etiqueta en controlador y modificacion de url al notebook

--- ClimbCodeDjango/climbcode/web/views.py
+++ ClimbCodeDjango/climbcode/web/views.py
@@ -212,7 +212,6 @@ def saveNotebook(request):
         }
         return HttpResponse(template.render(context, request))
 
-@csrf_exempt
 def createNotebook(request):
     """
     Muestra un formulario para crear un ejercicio y la crea si la petición es POST
@@ -239,7 +238,7 @@ def createNotebook(request):
                                                promoted=False, draft = True, level=level, category=category)
 
             idNotebook = exercise.id
-            template = loader.get_template('web/notebookv1.html')
+            template = loader.get_template('/web/editNotebook?idNotebook='+idNotebook)
             data={
                 'idNotebook':idNotebook
             }

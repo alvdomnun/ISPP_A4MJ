5ef988f09dc365d10f2273cbb6cc9e20ae861315
Miguel Baños <mabanos@gmail.com>
2018-04-26 03:23:07 +0200

    Quitada seguridad de acceso a showNotebook para realizar la demo
    issue #177

--- ClimbCodeDjango/climbcode/web/views.py
+++ ClimbCodeDjango/climbcode/web/views.py
@@ -466,13 +466,13 @@ def isSchoolAdquiredExercise(exerciseParam,school):
 # Visualización notebook para escuelas, profesores y alumnos
 
 def showNotebook(request):
-    #TODO Controlar los ataques por GET
     print("Showing notebook")
     if request.method == 'GET':
         # Petición de edición de notebook existente
         idNotebook = request.GET.get('idNotebook')
         exercise = Exercise.objects.get(id=idNotebook)
-        if (permisoViewRolesEscuelaNotebook(idNotebook,request) and exercise.draft == False or permisoViewRolProgramadorNotebook(idNotebook,request)):
+        #if (permisoViewRolesEscuelaNotebook(idNotebook,request) and exercise.draft == False or permisoViewRolProgramadorNotebook(idNotebook,request)):
+        if True:
             print("El título del notebook recuperado es: "+exercise.title)
             if exercise is not None:
                 template = loader.get_template('notebook/show_notebook.html')

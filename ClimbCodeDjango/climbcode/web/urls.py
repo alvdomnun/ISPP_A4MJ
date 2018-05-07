aedfad58b48978d6f453c4bd96b2cd0eb2681eae
jessossan <jesosa94@gmail.com>
2018-05-07 11:47:27 +0200

    eliminados todas las plantillas,funciones y urls que no se utilizaban #209

--- ClimbCodeDjango/climbcode/web/urls.py
+++ ClimbCodeDjango/climbcode/web/urls.py
@@ -6,16 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sampleDashboard', views.sample_dashboard, name="sampleDashboard"),
-    path('notebookVistaV1', views.notebookVistaV1, name='notebookVistaV1'),
-    path('notebookv1', views.notebookv1, name='notebookv1'),
-    path('notebookv1_ejercicio_creado', views.notebookv1_ejercicio_creado, name='notebookv1_ejercicio_creado'),
-    path('notebookv1_ejercicio_cc', views.notebookv1_ejercicio_cc, name='notebookv1_ejercicio_cc'),
-    path('notebookv1_ejercicio_am', views.notebookv1_ejercicio_am, name='notebookv1_ejercicio_am'),
-    path('notebookv1_ejercicio_qin', views.notebookv1_ejercicio_qin, name='notebookv1_ejercicio_qin'),
-    path('notebookv1_ejercicio_est', views.notebookv1_ejercicio_est, name='notebookv1_ejercicio_est'),
-    path('notebookv1aux', views.notebookv1aux, name='notebookv1aux'),
-    path('notebookv1_ejercicio_geo', views.notebookv1_ejercicio_geo, name='notebookv1_ejercicio_geo'),
 
     url(r'^pruebaAjaxNotebook$', views.pruebaAjaxNotebook, name='pruebaAjaxNotebook'),
     url(r'^saveNotebook$', views.saveNotebook, name='saveNotebook'),

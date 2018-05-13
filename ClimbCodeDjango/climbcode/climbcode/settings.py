d7c0af7354cee305cc3b70e4934214e567d45d95
Andrés Fernández Alés <andferale@alum.us.es>
2018-05-13 17:53:49 +0200

    #233 Actualizado settings para aceptar interacteach.tk

--- ClimbCodeDjango/climbcode/climbcode/settings.py
+++ ClimbCodeDjango/climbcode/climbcode/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-v))azfkfi5bkisq3*l#t-p6#)xs$^dv^8#4xg*u1gno2jyzna'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.cabesa.tk', 'cabesa.tk', '178.62.10.40', '159.89.103.249', 'climbcode.tk', 'www.climbcode.tk',
+ALLOWED_HOSTS = ['www.interacteach.tk', 'interacteach.tk', '178.62.10.40', '159.89.103.249', 'climbcode.tk', 'www.climbcode.tk',
                  '.climbcode.tk', '127.0.0.1']
 
 FILE_UPLOAD_HANDLERS = [
